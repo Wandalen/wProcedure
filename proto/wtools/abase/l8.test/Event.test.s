06d5f9a7b613a751b33843536992eb0a708890af
Dmytro <dm.vict.kr@gmail.com>
2020-11-27 11:38:36 +0200

    Add test suite `Event.test.s`, write test routine `onWithMap`

---
+++ proto/wtools/abase/l8.test/Event.test.s
@@ -0,0 +1,176 @@
+( function _Event_test_s_()
+{
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  let _ = require( '../../../wtools/Tools.s' );
+
+  _.include( 'wTesting' );
+  _.include( 'wConsequence' );
+
+  require( '../l8_procedure/Include.s' );
+
+}
+
+let _global = _global_;
+let _ = _global_.wTools;
+
+// --
+//
+// --
+
+function onWithMap( test )
+{
+  var self = this;
+
+  /* - */
+
+  test.open( 'option first - 0' );
+
+  test.case = 'no callback for events';
+  var result = [];
+  var onEvent = () => result.push( result.length );
+  var onEvent2 = () => result.push( -1 * result.length );
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationEnd' );
+  test.identical( result, [] );
+
+  /* */
+
+  test.case = 'single callback for single event, single event is given';
+  var result = [];
+  var onEvent = () => result.push( result.length );
+  var onEvent2 = () => result.push( -1 * result.length );
+  var got = _.procedure.on({ 'callbackMap' : { 'terminationBegin' : onEvent } });
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ 0 ] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationEnd' );
+  test.identical( result, [ 0 ] );
+  test.true( _.event.eventHasHandler( _.procedure._ehandler, { eventName : 'terminationBegin', eventHandler : onEvent } ) );
+  test.false( _.event.eventHasHandler( _.procedure._ehandler, { eventName : 'terminationEnd', eventHandler : onEvent2 } ) );
+  got.off();
+
+  /* */
+
+  test.case = 'single callback for single event, a few events are given';
+  var result = [];
+  var onEvent = () => result.push( result.length );
+  var onEvent2 = () => result.push( -1 * result.length );
+  var got = _.procedure.on({ 'callbackMap' : { 'terminationBegin' : onEvent }} );
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ 0 ] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ 0, 1 ] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationEnd' );
+  test.identical( result, [ 0, 1 ] );
+  test.true( _.event.eventHasHandler( _.procedure._ehandler, { eventName : 'terminationBegin', eventHandler : onEvent } ) );
+  test.false( _.event.eventHasHandler( _.procedure._ehandler, { eventName : 'terminationEnd', eventHandler : onEvent2 } ) );
+  got.off();
+
+  /* */
+
+  test.case = 'single callback for each events in event handler, a few events are given';
+  var result = [];
+  var onEvent = () => result.push( result.length );
+  var onEvent2 = () => result.push( -1 * result.length );
+  var got = _.procedure.on({ 'callbackMap' : { 'terminationBegin' : onEvent, 'terminationEnd' : onEvent2 } });
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ 0 ] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ 0, 1 ] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationEnd' );
+  _.event.eventGive( _.procedure._ehandler, 'terminationEnd' );
+  test.identical( result, [ 0, 1, -2, -3 ] );
+  test.true( _.event.eventHasHandler( _.procedure._ehandler, { eventName : 'terminationBegin', eventHandler : onEvent } ) );
+  test.true( _.event.eventHasHandler( _.procedure._ehandler, { eventName : 'terminationEnd', eventHandler : onEvent2 } ) );
+  got.off();
+
+  test.close( 'option first - 0' );
+
+  /* - */
+
+  test.open( 'option first - 1' );
+
+  test.case = 'callback added before other callback';
+  var result = [];
+  var onEvent = () => result.push( result.length );
+  var onEvent2 = () => result.push( -1 * result.length );
+  var got = _.procedure.on({ 'callbackMap' : { 'terminationBegin' : onEvent } });
+  var got2 = _.procedure.on({ 'callbackMap' : { 'terminationBegin' : onEvent2 }, 'first' : 1 });
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ -0, 1 ] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ -0, 1, -2, 3 ] );
+  got.off();
+  got2.off();
+
+  /* */
+
+  test.case = 'callback added after other callback';
+
+  var result = [];
+  var onEvent = () => result.push( result.length );
+  var onEvent2 = () => result.push( -1 * result.length );
+  var got = _.procedure.on({ 'callbackMap' : { 'terminationBegin' : onEvent2 }, 'first' : 1 });
+  var got2 = _.procedure.on({ 'callbackMap' : { 'terminationBegin' : onEvent } });
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ -0, 1 ] );
+  _.event.eventGive( _.procedure._ehandler, 'terminationBegin' );
+  test.identical( result, [ -0, 1, -2, 3 ] );
+
+  test.close( 'option first - 1' );
+
+  /* - */
+
+  if( !Config.debug )
+  return;
+
+  test.case = 'without arguments';
+  test.shouldThrowErrorSync( () => _.procedure.on() );
+
+  test.case = 'wrong type of callback';
+  test.shouldThrowErrorSync( () => _.procedure.on( 'terminationBegin', {} ) );
+
+  test.case = 'wrong type of event name';
+  test.shouldThrowErrorSync( () => _.procedure.on( [], () => 'str' ) );
+
+  test.case = 'wrong type of options map o';
+  test.shouldThrowErrorSync( () => _.procedure.on( 'wrong' ) );
+
+  test.case = 'extra options in options map o';
+  test.shouldThrowErrorSync( () => _.procedure.on({ callbackMap : {}, wrong : {} }) );
+
+  test.case = 'not known event in callbackMap';
+  test.shouldThrowErrorSync( () => _.procedure.on({ callbackMap : { unknown : () => 'unknown' } }) );
+}
+
+// --
+// declare
+// --
+
+let Self =
+{
+
+  name : 'Tools.procedure.Event',
+  silencing : 1,
+  enabled : 1,
+
+  tests :
+  {
+
+    onWithMap,
+
+  },
+
+};
+
+Self = wTestSuite( Self );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+})();
+
