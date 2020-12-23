4aa3cd686c469f04d7bdafe3a18571f0c43b0dc9
wandalen <wandalen.me@gmail.com>
2020-12-23 08:06:13 +0200

    .

--- proto/wtools/abase/l8_procedure/Procedure.s
+++ proto/wtools/abase/l8_procedure/Procedure.s
@@ -1124,8 +1124,9 @@ _.Copyable.mixin( Self );
 _.assert( _.routineIs( _.accessor.define.getter.alias ) );
 _.assert( _.routineIs( _.accessor.define.suite.alias ) );
 
-let alias = ( originalName ) => _.accessor.define.suite.alias({ originalName, container : Self });
-// let alias = ( originalName ) => _.define.alias({ originalName, originalContainer : Self });
+// let alias = ( originalName ) => _.accessor.define.suite.alias({ originalName, container : Self });
+// let alias = ( originalName ) => _.define.alias( originalName );
+let alias = ( originalName ) => _.define.alias({ originalName, originalContainer : Self });
 let join = ( originalName ) => _.routineJoin( Self, Self[ originalName ] );
 let NamespaceBlueprint =
 {
