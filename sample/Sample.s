7b312ec9a74cc72d7bed0c0af5b90c4f7faf3cfe
Dmytro <dm.vict.kr@gmail.com>
2021-01-22 10:42:52 +0200

    Fix production version of module, add missed dependency `wProto`

--- sample/Sample.s
+++ sample/Sample.s
@@ -1,12 +1,9 @@
 
 let procedure = require( 'wprocedure' );
 
-/**/
+/* */
 
-debugger;
 procedure.begin( 'some procedure' );
 procedure.terminationReport();
-debugger;
 procedure.end( 'some procedure' );
-debugger;
 procedure.terminationReport();
